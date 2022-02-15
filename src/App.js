cd55f3c96034ec97052481c733cd1bde01e1902d
unknown <bmika@icloud.com>
2022-02-15 17:53:50 -0500

    Base structure of project and new library `react-router-dom` installed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import "./w3.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import Index from "./Components/Gateway/Index";
+import NotFoundError from "./Components/Misc/NotFoundError";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <div>
+                    {/* <TopNavigation /> */}
+                </div>
+                <Routes>
+                    <Route exact path="/" element={<Index/>}/>
+                    <Route path="*" element={<NotFoundError/>}/>
+                </Routes>
+                <div>
+                   {/* DEVELOPERS NOTE: Mobile tab-bar menu can go here */}
+                </div>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/Misc/NotFoundError.jsx
@@ -0,0 +1,7 @@
+export default function NotFoundError(props){
+    return (
+        <>
+            <p>Page not found</p>
+        </>
+    );
+}
