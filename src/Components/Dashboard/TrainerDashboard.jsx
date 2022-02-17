396311db2d31b909dc1d7f2c859dbcb61ac54496
unknown <bmika@icloud.com>
2022-02-17 16:29:37 -0500

    BUGFIX: Fixed importing the `react-big-calendar`.

--- src/Components/Dashboard/TrainerDashboard.jsx
+++ src/Components/Dashboard/TrainerDashboard.jsx
@@ -1,8 +1,32 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+
 export default function TrainerDashboard (props){
+
+    const events = [
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "Some title"
+      }
+  ];
+
   return(
     <>
       <h1>Trainer Dashboard</h1>
-      <p>im just a random comment</p>
+      <Calendar
+       localizer={localizer}
+       defaultDate={new Date()}
+       defaultView="month"
+       events={events}
+       style={{ height: "100vh" }}
+     />
     </>
   )
 }
