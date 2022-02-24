c8a23202dd0ff8c8debd59e76fb6b78a5dfe45ca
Bartlomiej Mika <bmika@icloud.com>
2022-02-24 16:07:22 -0500

    BUGFIX: Helped Nick with `recharts.js` library.

--- src/Components/Clients/ClientDetails.jsx
+++ src/Components/Clients/ClientDetails.jsx
@@ -1,54 +1,20 @@
 import { React } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { LineChart, Line } from 'recharts';
 
 
 export default function ClientDetails(props) {
+
+    const data = [
+      { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+      { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+      { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+      { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+      { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+      { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+      { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 }
+    ];
+    // const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
+
     return (
         <>
         <div className="w3-row w3-padding-top-64">
@@ -83,9 +49,9 @@ export default function ClientDetails(props) {
            </div>
         </div>
         <div className="w3-row">
-           <div className="w3-col m1 w3-padding">
+           <div className="w3-col m1 w3-container">
            </div>
-           <div className="w3-col m6 w3-padding">
+           <div className="w3-col m6 w3-container">
               <h3>Goals</h3>
               <ul>
                  <li>Get clear overview of nutritional information information for his meals.</li>
@@ -98,13 +64,20 @@ export default function ClientDetails(props) {
               </ul>
            </div>
            <div className="w3-col m5">
-           <h1>Graph</h1>
-           <ResponsiveContainer width="100%" height="100%">
-             <LineChart width={300} height={100} data={data}>
-               <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-             </LineChart>
-           </ResponsiveContainer>
-           </div>
+                <div className="w3-row w3-margin-top">
+                     <div className="w3-col s1 w3-container">
+                     </div>
+                     <div className="w3-col s10 w3-container">
+                         {/* https://recharts.org/en-US/guide/getting-started */}
+                         <LineChart width={300} height={300} data={data} className="" style={{width:"100%"}}>
+                            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                         </LineChart>
+                     </div>
+                     <div className="w3-col s1 w3-container">
+                     </div>
+
+                </div>
+            </div>
         </div>
         </>
     );
