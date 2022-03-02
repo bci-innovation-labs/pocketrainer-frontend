3c4a4fa20e3d0fb2862bb9d78744cd64d6b9d2a3
Sebastian Cosme M <71041769+scosme926@users.noreply.github.com>
2022-03-02 15:22:23 -0500

    Last Update

--- src/Components/Appointments/Add.jsx
+++ src/Components/Appointments/Add.jsx
@@ -8,6 +8,7 @@ import { DatePickerCalendar } from 'react-nice-dates'
 import 'react-nice-dates/build/style.css'
 import { getDay } from 'date-fns'
 import Select from 'react-select'
+import { groupExerciseOptions, chestMuscleExerciseOptions, armMuscleExerciseOptions, legMuscleExerciseOptions, abdominalMuscleExerciseOptions } from '../../Constants/Fields'
 
 
 
@@ -15,14 +16,30 @@ class AddAppointment extends Component{
   constructor(props){
     super(props)
     this.state = {
+      specific_exercise_is_disabled: true,
       date:new Date(localStorage.getItem("USER_PICKED_DATE")),
-      options : [
-      { value: 'chocolate', label: 'Chocolate' },
-      { value: 'strawberry', label: 'Strawberry' },
-      { value: 'vanilla', label: 'Vanilla' }
+      client_options : [
+      { value: '1', label: 'Adam' },
+      { value: '2', label: 'Jacob' },
+      { value: '3', label: 'Ana' }
+    ],
+      specificExerciseOptions : [],
+      reps : "",
+      series : "",
+      routines : [
+        {
+          groupExercise : { value: 'chest_muscle', label: 'Chest Muscle' },
+          specificExercise : { value: 'a', label: 'A' },
+          reps : 50,
+          series : 4,
+
+        }
       ]
     };
     this.onDateChange = this.onDateChange.bind(this)
+    this.onGroupExerciseChange = this.onGroupExerciseChange.bind(this)
+    this.onRepsChange = this.onRepsChange.bind(this)
+    this.onSeriesChange = this.onSeriesChange.bind(this)
   }
 
   onDateChange(pickedDate){
@@ -33,13 +50,47 @@ class AddAppointment extends Component{
     localStorage.setItem("USER_PICKED_DATE", pickedDate);
   }
 
+  onGroupExerciseChange(e){
+    console.log(e)
+    let specificExerciseOptions;
+    if (e.value === "chest_muscle") {
+      specificExerciseOptions = chestMuscleExerciseOptions
+    }
+    else if (e.value === "arm_muscle") {
+      specificExerciseOptions = armMuscleExerciseOptions
+    }
+    else if (e.value === "leg_muscle") {
+      specificExerciseOptions = legMuscleExerciseOptions
+    }
+    else if (e.value === "abdonimal_muscle") {
+      specificExerciseOptions = abdominalMuscleExerciseOptions
+    }
+    this.setState({
+      groupExercise:e,
+      specificExerciseOptions : specificExerciseOptions,
+      specific_exercise_is_disabled: false,
+    })
+  }
 
+  onRepsChange(e){
+    const value = e.target.value;
+    this.setState({
+      reps:value
+    })
+  }
+
+  onSeriesChange(e){
+    const value = e.target.value;
+    this.setState({
+      series:value
+    })
+  }
 
   render(){
-    const { options } = this.state
-    console.log(options)
+    const { client_options, specificExerciseOptions, reps, series, routines } = this.state
+    console.log(routines)
     const { onDateChange } = this.state
-    const { date } = this.state
+    const { date, specific_exercise_is_disabled } = this.state
     //*new Date().setDate(new Date().getDate() - 1* === Yesterday Day
     const modifiers = {
       disabled: date => (date <  new Date().setDate(new Date().getDate() - 1)) || (getDay(date) === 0),
@@ -85,39 +136,60 @@ class AddAppointment extends Component{
       <>
         <h1 className="w3-center">Add Appointment</h1>
         <div className="w3-row">
-          <div className="w3-half w3-container">
+           <div className="w3-col l6">
+            <h3 className="w3-padding">Client</h3>
             <form className="w3-container">
-              <Select styles={customStyles} options={options} placeholder={'Select Customer'} />
+              <p><Select styles={customStyles} options={client_options} placeholder={'Select Customer'} /></p>
             </form>
           <br />
           <DatePickerCalendar date={date} onDateChange={this.onDateChange} locale={enGB} modifiers={modifiers} />
           </div>
-          <div className="w3-half w3-container">
-            <div className="w3-cell w3-center" style={{width:"30%"}}>
-                <input className="w3-check" type="checkbox" />
-                <img className="w3-padding w3-center w3-round-large" src="Abdomen .jpeg" style={{width:"300px", height:"150px"}}></img>
-            </div>
-          </div>
-          <div className="w3-half w3-container">
-            <div className="w3-cell w3-center" style={{width:"30%"}}>
-                <input className="w3-check" type="checkbox" />
-                <label className="w3-text-blue"><b>Client</b></label>
+          <div className="w3-col l6">
+            <h3 className="w3-padding">Exercise Plan</h3>
+            {/* start form */}
+            <form className="w3-container">
+              <p><Select styles={customStyles} options={groupExerciseOptions} placeholder={'Group Exercise'} onChange={this.onGroupExerciseChange} /></p>
+              <p><Select styles={customStyles} options={specificExerciseOptions} placeholder={'Specific Exercise'} isDisabled={specific_exercise_is_disabled} /></p>
+              <p>
+                  <input className="w3-input w3-border w3-black w3-border-purple w3-round" name="first" type="text" placeholder="Reps" onChange={this.onRepsChange} value={reps} /></p>
+                  <p>
+                  <input className="w3-input w3-border w3-black w3-border-purple w3-round" name="last" type="text" placeholder="Series" onChange={this.onSeriesChange} value={series} /></p>
+              <p><button className="w3-button w3-block w3-white w3-round-large w3-text-purple"> <b><i className="fa fa-plus"></i> Add</b></button></p>
+            </form>
+            {/* end form */}
+          {/* begin table */}
+          <div className="w3-padding">
+          <h3 className="w3-padding">Routine</h3>
+          <table className="w3-table w3-black w3-border w3-border-purple w3-centered">
+            <tr>
+              <th style={{width:"30%"}}>Group Exercise</th>
+              <th>Specific Exercise</th>
+              <th>Reps</th>
+              <th>Series</th>
+            </tr>
+            {routines.map((item,key)=>
+               (
+                <tr key={key}>
+                <td>{item.groupExercise.label}</td>
+                <td>{item.specificExercise.label}</td>
+                <td>{item.reps}</td>
+                <td>{item.series}</td>
+              </tr>
 
-                <img className="w3-padding w3-center w3-round-large" src="Arm.jpeg" style={{width:"300px", height:"150px"}}></img>
-            </div>
+              ))}
+            <tr>
+              <td>Arm</td>
+              <td>Push Up</td>
+              <td>50</td>
+              <td>4</td>
+            </tr>
+          </table>
           </div>
-          <div className="w3-half w3-container">
-            <div className="w3-cell w3-center" style={{width:"30%"}}>
-                <input className="w3-check" type="checkbox" />
-                <img className="w3-padding w3-center w3-round-large" src="chest.jpeg" style={{width:"300px", height:"150px"}}></img>
-            </div>
+          {/* end table */}
           </div>
-          <div className="w3-half w3-container">
-            <div class="w3-display-container w3-text-white">
-              <input className="w3-check" type="checkbox" />
-              <img src="Leg.jpeg" alt="Lights" style={{width:"300px", height:"150px"}}></img>
-              <div class="w3-display-left w3-container w3-padding"><p>Hola</p></div>
-            </div>
+          <br/>
+          <div className="w3-padding w3-right">
+            <button class="w3-button w3-purple w3-round w3-large">Submit</button>
           </div>
         </div>
       </>
---
+++ src/Constants/Fields.js
@@ -0,0 +1,30 @@
+export const groupExerciseOptions = [
+{ value: 'chest_muscle', label: 'Chest Muscle' },
+{ value: 'abdonimal_muscle', label: 'Abdonimal Muscle' },
+{ value: 'leg_muscle', label: 'Leg Muscle' },
+{ value: 'arm_muscle', label: 'Arm Muscle' }
+];
+export const chestMuscleExerciseOptions = [
+{ value: 'a', label: 'A' },
+{ value: 'b', label: 'B' },
+{ value: 'c', label: 'C' },
+{ value: 'd', label: 'D' }
+];
+export const abdominalMuscleExerciseOptions = [
+{ value: 'e', label: 'E' },
+{ value: 'f', label: 'F' },
+{ value: 'g', label: 'G' },
+{ value: 'h', label: 'H' }
+];
+export const legMuscleExerciseOptions = [
+{ value: 'j', label: 'J' },
+{ value: 'k', label: 'K' },
+{ value: 'l', label: 'L' },
+{ value: 'm', label: 'M' }
+];
+export const armMuscleExerciseOptions = [
+{ value: 'n', label: 'N' },
+{ value: 'o', label: 'O' },
+{ value: 'p', label: 'P' },
+{ value: 'q', label: 'Q' }
+];
