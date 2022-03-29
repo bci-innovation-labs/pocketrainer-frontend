820330051363e7de6977477a4805a3c30baa73fb
Nicolas Arias Lopez <71041867+NikoArias@users.noreply.github.com>
2022-03-29 18:05:16 -0400

    ClientProfile succesfully conected with Backend

--- src/Components/Clients/ClientProfile.jsx
+++ src/Components/Clients/ClientProfile.jsx
@@ -1,7 +1,27 @@
-import { React } from "react";
+import { React, useState } from "react";
+import { BIOMETRICS_CLOUD_LOGIN_URL } from "../../Constants/App"
+import { Link, Navigate } from "react-router-dom"
+
+import { getProfileDetail } from "../../API/profile";
+
 
 
 export default function ClientProfile(props) {
+  const [profile, setProfile] = useState(JSON.parse(localStorage.getItem("profile")))
+
+  function onGetProfileSucces(response){
+     const profile = JSON.stringify(response.data)
+     localStorage.setItem("profile", profile)
+   }
+
+  function onGetProfileError(err){
+   }
+
+  function onGetProfileDone(){
+   }
+
+   getProfileDetail(onGetProfileSucces, onGetProfileError, onGetProfileDone)
+
     return (
         <>
         <div className="w3-row">
@@ -10,7 +30,7 @@ export default function ClientProfile(props) {
             <p></p>
          </div>
          <div className="w3-col m4 w3-center">
-            <img src="IconExample.png" className="w3-circle w3-center w3-padding-top-64" style={{width:"35%", height:"25%"}}/>
+          {/*  <img src="IconExample.png" className="w3-circle w3-center w3-padding-top-64" style={{width:"35%", height:"25%"}}/> */}
          </div>
          <div className="w3-col m4">
             <p></p>
@@ -21,30 +41,26 @@ export default function ClientProfile(props) {
          <div className="w3-col m6">
             <div className="w3-container w3-margin-left w3-padding-32">
                <span className="w3-text-grey">Hello</span>
-               <h4><strong>Fabian Levy</strong></h4>
+               <h4><strong>{profile.firstName}</strong></h4>
             </div>
             <div className="w3-container w3-margin-left w3-padding-32">
                <span className="w3-text-grey">Age</span>
-               <h4><strong>23</strong></h4>
+               <h4><strong>{profile.age}</strong></h4>
             </div>
             <div className="w3-container w3-margin-left w3-padding-32">
                <span className="w3-text-grey">Occupation</span>
-               <h4><strong>Web Developer</strong></h4>
+               <h4><strong>{profile.occupation}</strong></h4>
             </div>
             <div className="w3-container w3-margin-left w3-padding-32">
                <span className="w3-text-grey">Hobbies</span>
-               <h4><strong>Rock climbing, board games.</strong></h4>
+               <h4><strong>{profile.hobbies}</strong></h4>
             </div>
          </div>
          <div className="w3-col m6">
             <div className="w3-row">
                <div className="w3-container w3-margin-left w3-padding-32">
                   <h3><strong>Goals</strong></h3>
-                  <ul className="">
-                     <li>Get clear overview of nutritional information for his meals. </li>
-                     <li>Set diet goals.</li>
-                     <li>Track progress and meet deadlines.</li>
-                  </ul>
+                  <h4><strong>{profile.goals}</strong></h4>
                </div>
             </div>
             <div className="w3-row">
--- src/Constants/App.js
+++ src/Constants/App.js
@@ -1,4 +1,4 @@
 export const TRAINER_ROLE_ID = 1;
 export const CLIENT_ROLE_ID = 2;
-export const BIOMETRICS_CLOUD_LOGIN_URL = "https://biometricscloud.net/authorize?client_id=a761085070124b1c&redirect_uri=https%3A%2F%2Fd76b-2607-fea8-2aa1-7d00-9894-aae4-6db7-6d73.ngrok.io%2Fappauth%2Fcode&response_type=code&scope=all"
+export const BIOMETRICS_CLOUD_LOGIN_URL = "http://biometricscloud.net/authorize?client_id=11f8053d3859cc24&redirect_uri=https%3A%2F%2F584b-2607-fea8-2aa1-7d00-4d65-966c-647b-1565.ngrok.io%2Fappauth%2Fcode&response_type=code&scope=all"
 export const PROFILE_UPDATE_ENDPOINT = "http://127.0.0.1:8000/profile"
