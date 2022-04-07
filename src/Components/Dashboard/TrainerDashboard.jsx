6e0e5d8202fc91c9f4baa62db0ad5b90cb3658da
Nicolas Arias Lopez <71041867+NikoArias@users.noreply.github.com>
2022-04-07 16:28:15 -0400

    Appoiment List Integrated with Backend, Calendar not working.

--- src/Components/Dashboard/TrainerDashboard.jsx
+++ src/Components/Dashboard/TrainerDashboard.jsx
@@ -1,58 +1,79 @@
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import { React , useState, useEffect } from "react";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { getAppoimentList } from "../../API/appoiments";
+
 
 const localizer = momentLocalizer(moment);
 
 
 export default function TrainerDashboard (props){
+    const [appoiments, setAppoiments] = useState([])
+
+
+  function onGetAppoimentSucces(response){
+  console.log(response.data)
+  setAppoiments(response.data)
+
+  }
+  function onGetAppoimentError(err){
+
+  }
+  function onGetAppoimentDone(){
+
+  }
+
+  useEffect(() => {
+  setTimeout(() => {
+    getAppoimentList(onGetAppoimentSucces, onGetAppoimentError, onGetAppoimentDone)
+  }, 100)
+   return () => console.log('unmounting...');
+ }, [])
 
     const events = [
       {
+        id: 0,
         start: moment("2022-02-17 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #1"
       },
       {
+        id: 1,
         start: moment("2022-02-17 10:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #12"
       },
       {
+        id: 2,
         start: moment("2022-02-17 12:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 13:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #13"
       },
       {
+        id: 3,
         start: moment("2022-02-17 15:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 16:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #14"
       },
       {
+        id: 4,
         start: moment("2022-02-17 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #15"
       },
       {
+        id: 5,
         start: moment("2022-02-17 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #16"
       },
       {
+        id: 6,
         start: moment("2022-02-17 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         end: moment("2022-02-17 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
         title: "Workout #17"
       },
-      {
-        start: moment("2022-02-25 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-        end: moment("2022-02-25 10:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-        title: "Workout #3"
-      },
-      {
-        start: moment("2022-02-18 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-        end: moment("2022-02-18 10:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-        title: "Workout #2"
-      }
   ];
 
   return(
@@ -65,7 +86,7 @@ export default function TrainerDashboard (props){
        localizer={localizer}
        defaultDate={new Date()}
        defaultView="month"
-       events={events}
+       events={null}
        style={{ height: "100vh" }}
      />
     </>
