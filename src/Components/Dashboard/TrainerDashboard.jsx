250ef6936e550920a9032f15d9f8f7d310a06cbf
Nicolas Arias Lopez <71041867+NikoArias@users.noreply.github.com>
2022-04-11 15:57:37 -0400

    Further updates

--- src/Components/Dashboard/TrainerDashboard.jsx
+++ src/Components/Dashboard/TrainerDashboard.jsx
@@ -1,32 +1,25 @@
 import { Calendar, momentLocalizer } from "react-big-calendar";
-import { React , useState, useEffect } from "react";
+import { React , useState, useEffect, useCallback } from "react";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { getAppoimentList } from "../../API/appoiments";
+import { Link, Navigate } from "react-router-dom"
+
 
 
 const localizer = momentLocalizer(moment);
-
-/*const events = [
-  {
-    id: 0,
-    start: moment("2022-04-16 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-    end: moment("2022-04-17 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-    title: "Workout #1"
-  },
-  {
-    id: 1,
-    start: moment("2022-04-18 09:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-    end: moment("2022-04-19 11:00:00", "YYYY-MM-DD hh:mm:ss").toDate(),
-    title: "Workout #2"
-  },
-];*/
-
 export default function TrainerDashboard (props){
     const [appoiments, setAppoiments] = useState([])
     const [events, setEvents] = useState({})
+    const [forceURL, setForceURL] = useState("")
 
-
+    const handleSelectEvent = useCallback(
+       (event) => {
+         console.log(event.id)
+         setForceURL("/appoiment/" + event.id)
+       },
+       []
+     )
   function onGetAppoimentSucces(response){
     let e = []
 
@@ -57,8 +50,11 @@ export default function TrainerDashboard (props){
    return () => console.log('unmounting...');
  }, [])
 
-
-
+  if (forceURL !== ""){
+   return(
+   <Navigate to={forceURL}/>
+   )
+  }
   return(
     <>
       <div class="w3-display-container">
@@ -68,6 +64,7 @@ export default function TrainerDashboard (props){
        popup ={true}
        localizer={localizer}
        defaultDate={new Date()}
+       onSelectEvent={handleSelectEvent}
        defaultView="month"
        events={events}
        style={{ height: "100vh" }}
