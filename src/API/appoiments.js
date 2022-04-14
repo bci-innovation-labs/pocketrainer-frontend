087fe6fcc0a4f60a024d10f095d285be05417503
Nicolas Arias Lopez <71041867+NikoArias@users.noreply.github.com>
2022-04-14 17:09:44 -0400

    Client Notifications Succesfully integrated

--- src/API/appoiments.js
+++ src/API/appoiments.js
@@ -9,6 +9,12 @@ export function getAppoimentList(onGetAppoimentSucces, onGetAppoimentError, onGe
   if (filters.trainer_id !== undefined && filters.trainer_id !== null && filters.trainer_id !== 0) {
     url += "?trainer_id=" + filters.trainer_id
   }
+  else if (filters.client_id !== undefined && filters.client_id !== null && filters.client_id !== 0) {
+    url += "?client_id=" + filters.client_id
+  }
+  if (filters.has_client_read !== undefined && filters.has_client_read !== null) {
+    url += "&has_client_read=" + filters.has_client_read
+  }
   const a = getDefaultAxios()
   a.get(url)
   .then(onGetAppoimentSucces)
